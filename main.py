4eb1937bab84ca32c60a9fb66df06b236e84d55f
Michael <mrnka.michael@gmail.com>
2021-12-14 21:01:24 +0100

    v2.0

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ garpike and stingray are also present.'''
 users_pass = {'Bob': '123', 'Ann': 'pass123', 'Mike': 'password123', 'Liz': 'pass123'}
 number_of_texts = len(TEXTS)
 line = "-" * 35
-text_1 = TEXTS[0].strip(" , . : ! ? - _ ")
+text_1 = TEXTS[0]
 text_2 = TEXTS[1].strip(" , . : ! ? - _ ")
 text_3 = TEXTS[2].strip(" , . : ! ? - _ ")
 title_text = "LEN|  OCCURENCES  |NR."
@@ -64,31 +64,20 @@ else:
 
 if choice == "1":
     # number of words
-    print(f"There are {len(text_1.split())} words in the selected text.")
+    print(f"There are {sum(1 if word_1.isalnum() or word_1.istitle() or word_1.islower() else 0 for word_1 in text_1.split())} words in the selected text.")
+
     # number of titlecase
-    num_of_title_1 = 0
-    for title_1 in text_1.split():
-        if title_1.istitle():
-            num_of_title_1 += 1
-    print(f"There are {num_of_title_1} titlecase words.")
+    print(f"There are {sum(1 if title_1.istitle() else 0 for title_1 in text_1.split())} titlecase words.")
+
     # number of uppercase
-    num_of_upper_1 = 0
-    for upper_1 in text_1.split():
-        if upper_1.isupper() and upper_1.isalpha():
-            num_of_upper_1 += 1
-    print(f"There are {num_of_upper_1} uppercase words.")
+    print(f"There are {sum(1 if upper_1.isupper() and upper_1.isalpha() else 0 for upper_1 in text_1.split())} uppercase words.")
+
     # number of lowercase
-    num_of_lower_1 = 0
-    for lower_1 in text_1.split():
-        if lower_1.islower():
-            num_of_lower_1 += 1
-    print(f"There are {num_of_lower_1} lowercase words.")
+    print(f"There are {sum(1 if lower_1.islower() else 0 for lower_1 in text_1.split())} lowercase words.")
+
     # number of numeric strings
-    num_of_num_1 = 0
-    for num_1 in text_1.split():
-        if num_1.isnumeric():
-            num_of_num_1 += 1
-    print(f"There are {num_of_num_1} numeric strings.")
+    print(f"There are {sum(1 if num_1.isnumeric() else 0 for num_1 in text_1.split())} numeric strings.")
+
     #sum of all numeric
     sum_total_1 = 0
     for sum_1 in text_1.split():
@@ -96,15 +85,19 @@ if choice == "1":
             sum_total_1 += int(sum_1)
     print(f"The sum of all numbers {sum_total_1}.")
     print(line)
+
     #graph
     print("LEN|    OCCURENCES    |NR.")
     print(line)
+
     len_1 = {}
     for numbers_1 in text_1.split():
-        if len(numbers_1.strip(".,")) not in len_1:
-            len_1[len(numbers_1.strip(".,"))] = 1
+        if not numbers_1.strip(".,!:?").isalnum():
+            continue
+        elif len(numbers_1.strip(".,!:?")) not in len_1:
+            len_1[len(numbers_1)] = 1
         else:
-            len_1[len(numbers_1.strip(",."))] += 1
+            len_1[len(numbers_1.strip(".,!:?"))] += 1
     len_1 = sorted(len_1.items())
     for numbers_11 in len_1:
         lengt_graph = "*" * (numbers_11[1])
@@ -115,31 +108,20 @@ if choice == "1":
 
 if choice == "2":
     # number of words
-    print(f"There are {len(text_2.split())} words in the selected text.")
+    print(f"There are {sum(1 if word_2.isalnum() or word_2.istitle() or word_2.islower() else 0 for word_2 in text_2.split())} words in the selected text.")
+
     # number of titlecase
-    num_of_title_2 = 0
-    for title_2 in text_2.split():
-        if title_2.istitle():
-            num_of_title_2 += 1
-    print(f"There are {num_of_title_2} titlecase words.")
+    print(f"There are {sum(1 if title_2.istitle() else 0 for title_2 in text_2.split())} titlecase words.")
+
     # number of uppercase
-    num_of_upper_2 = 0
-    for upper_2 in text_2.split():
-        if upper_2.isupper() and upper_2.isalpha():
-            num_of_upper_2 += 1
-    print(f"There are {num_of_upper_2} uppercase words.")
+    print(f"There are {sum(1 if upper_2.isupper() and upper_2.isalpha() else 0 for upper_2 in text_2.split())} uppercase words.")
+
     # number of lowercase
-    num_of_lower_2 = 0
-    for lower_2 in text_2.split():
-        if lower_2.islower():
-            num_of_lower_2 += 1
-    print(f"There are {num_of_lower_2} lowercase words.")
+    print(f"There are {sum(1 if lower_2.islower() else 0 for lower_2 in text_2.split())} lowercase words.")
+
     # number of numeric strings
-    num_of_num_2 = 0
-    for num_2 in text_2.split():
-        if num_2.isnumeric():
-            num_of_num_2 += 1
-    print(f"There are {num_of_num_2} numeric strings.")
+    print(f"There are {sum(1 if num_2.isnumeric() else 0 for num_2 in text_2.split())} numeric strings.")
+
     # sum of all numeric
     sum_total_2 = 0
     for sum_2 in text_2.split():
@@ -147,47 +129,41 @@ if choice == "2":
             sum_total_2 += int(sum_2)
     print(f"The sum of all numbers {sum_total_2}.")
     print(line)
+
     #graph
     print("LEN|    OCCURENCES    |NR.")
     print(line)
+
     len_2 = {}
     for numbers_2 in text_2.split():
-        if len(numbers_2.strip(".,")) not in len_2:
-            len_2[len(numbers_2.strip(".,"))] = 1
+        if not numbers_2.strip(".,!:?").isalnum():
+            continue
+        elif len(numbers_2.strip(".,!:?")) not in len_2:
+            len_2[len(numbers_2)] = 1
         else:
-            len_2[len(numbers_2.strip(".,"))] += 1
-    len_1 = sorted(len_2.items())
-    for numbers_22 in len_1:
+            len_2[len(numbers_2.strip(".,!:?"))] += 1
+    len_2 = sorted(len_2.items())
+    for numbers_22 in len_2:
         lengt_graph = "*" * (numbers_22[1])
         print(f"{numbers_22[0]:>3}|{lengt_graph:<18}|{numbers_22[1]}")
 
 
 if choice == "3":
     # number of words
-    print(f"There are {len(text_3.split())} words in the selected text.")
-    num_of_title_3 = 0
-    for title_3 in text_3.split():
-        if title_3.istitle():
-            num_of_title_3 += 1
-    print(f"There are {num_of_title_3} titlecase words.")
+    print(f"There are {sum(1 if word_3.isalnum() or word_3.istitle() or word_3.islower() else 0 for word_3 in text_3.split())} words in the selected text.")
+
+    # number of titlecase
+    print(f"There are {sum(1 if title_3.istitle() else 0 for title_3 in text_3.split())} titlecase words.")
+
     # number of uppercase
-    num_of_upper_3 = 0
-    for upper_3 in text_3.split():
-        if upper_3.isupper() and upper_3.isalpha():
-            num_of_upper_3 += 1
-    print(f"There are {num_of_upper_3} uppercase words.")
+    print(f"There are {sum(1 if upper_3.isupper() and upper_3.isalpha() else 0 for upper_3 in text_3.split())} uppercase words.")
+
     # number of lowercase
-    num_of_lower_3 = 0
-    for lower_3 in text_3.split():
-        if lower_3.islower():
-            num_of_lower_3 += 1
-    print(f"There are {num_of_lower_3} lowercase words.")
+    print(f"There are {sum(1 if lower_3.islower() else 0 for lower_3 in text_3.split())} lowercase words.")
+
     # number of numeric strings
-    num_of_num_3 = 0
-    for num_3 in text_3.split():
-        if num_3.isnumeric():
-            num_of_num_3 += 1
-    print(f"There are {num_of_num_3} numeric strings.")
+    print(f"There are {sum(1 if num_3.isnumeric() else 0 for num_3 in text_3.split())} numeric strings.")
+
     # sum of all numerict
     sum_total_3 = 0
     for sum_3 in text_3.split():
@@ -195,15 +171,19 @@ if choice == "3":
             sum_total_3 += int(sum_3)
     print(f"The sum of all numbers {sum_total_3}.")
     print(line)
+
     # graph
     print("LEN|    OCCURENCES    |NR.")
     print(line)
+
     len_3 = {}
     for numbers_3 in text_3.split():
-        if len(numbers_3.strip(".,")) not in len_3:
-            len_3[len(numbers_3.strip(".,"))] = 1
+        if not numbers_3.strip(".,!:?").isalnum():
+            continue
+        elif len(numbers_3.strip(".,!:?")) not in len_3:
+            len_3[len(numbers_3)] = 1
         else:
-            len_3[len(numbers_3.strip(".,"))] += 1
+            len_3[len(numbers_3.strip(".,!:?"))] += 1
     len_3 = sorted(len_3.items())
     for numbers_33 in len_3:
         lengt_graph = "*" * (numbers_33[1])
